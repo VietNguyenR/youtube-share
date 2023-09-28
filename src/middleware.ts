10d4c2ce3e314a7f474884952836e3700a4ec3b6
Viet Nguyen <105207060+VietNguyenR@users.noreply.github.com>
2023-09-28 16:08:38 +0700

    Initial app with basic authentication

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const isAuthPage =
+      req.nextUrl.pathname.startsWith('/signin') ||
+      req.nextUrl.pathname.startsWith('/signup');
+
+    if (isAuthPage && isAuth) {
+      return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
+
+    if (!isAuthPage && !isAuth) {
+      return NextResponse.redirect(new URL('/signin', req.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true;
+      },
+    },
+  },
+);
